9654b02c53e416815014c05ce45c1fdf8e664aa9
flastar-fr <140191575+flastar-fr@users.noreply.github.com>
2026-07-01 17:24:12 +0200

    fix: recent anime displaying

--- src/gui/routers/api.py
+++ src/gui/routers/api.py
@@ -246,7 +246,7 @@ async def get_recent_anime():
                         <h3 class="card-title" title="{anime.title}">{anime.title}</h3>
                     </a>
                     <div class="card-info">
-                        <span>Épisode {anime.week_episode - 1}</span> <span>Saison {anime.season}</span>
+                        <span>{anime.season}</span>
                     </div>
                     <div class="card-status history">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
